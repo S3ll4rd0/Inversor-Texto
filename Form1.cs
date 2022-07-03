76a3bc7a0d837dc3f3c0f74007fd614c2b539201
Antonio Nicolás Salmerón Rubio <54323367+S3ll4rd0@users.noreply.github.com>
2022-07-03 23:01:29 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inversa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String texto = textBox1.Text.ToString();
+            string reverse = String.Empty;
+            char [] car = new char[texto.Length];
+            for (int i = 0; i < texto.Length; i++)
+            {
+                car[i] = texto[i];
+            }
+            for (int i = car.Length - 1; i >= 0; i--)
+            {
+                reverse += car[i];
+            }
+            label2.Text = reverse;
+        }
+    }
+}
